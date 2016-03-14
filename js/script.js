7790c96128e99afa3a47c6cfca3153d282e0656e
samvnkauffman-pc <samvnkauffman@gmail.com>
2016-03-14 16:04:10 -0400

    latest

--- js/script.js
+++ js/script.js
@@ -5,9 +5,17 @@
 
 var sen = "This those and the thing";
 
-var newSen = sen.split("t");
+var newSen = sen.split(" ").join(" ");
 
 console.log(newSen);
 
 //////////////////////////////
 
+var oldArray = ["milk", "cheese", "eggs", "ham"];
+
+var newSlice = oldArray.slice(1, 3)[0];
+//var newSlicer = oldArray.splice(0, 2, "bob")[0];
+
+//console.log(oldArray);
+//console.log(newSlice);
+//console.log(newSlicer);

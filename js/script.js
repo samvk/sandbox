29b86d5bfbab19b66ad3294e941d41d6edf4d6e9
samvnkauffman-laptop <samvnkauffman@gmail.com>
2016-03-15 15:56:23 -0400

    w

--- js/script.js
+++ js/script.js
@@ -1,38 +1,6 @@
 /*global $, document, Image, window, setTimeout, setInterval, clearInterval */
 
-//split v slice v splice
+var one = "1";
+var two = "2";
 
-//split is only for strings and returns arrays based off what it's using as the ',' prelacement
-//usually the space, but doesnt have to be
-
-var sen = "This those and the thing";
-
-var newSen = sen.split(" ").join(" ");
-
-console.log(newSen);
-
-//////////////////////////////
-
-var oldArray = ["milk", "cheese", "eggs", "ham"];
-
-var newSlice = oldArray.slice(1, 3)[0];
-//var newSlicer = oldArray.splice(0, 2, "bob")[0];
-
-//console.log(oldArray);
-//console.log(newSlice);
-//console.log(newSlicer);
-
-var sent = "";
-
-var a = "Part 1 ";
-var b = "Part 2 ";
-var c = "Part 3";
-console.time();
-sent += a;
-console.log(sent);
-sent += b;
-console.log(sent);
-sent += c;
-
-console.log(sent);
-console.timeEnd();
+console.log(+one + +two);

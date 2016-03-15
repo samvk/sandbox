7f67132945471044fc8e10b7d8dd2d618c8c201a
samvnkauffman-laptop <samvnkauffman@gmail.com>
2016-03-15 15:53:29 -0400

    a

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/*global $, document, Image, window, setTimeout, setInterval, clearInterval */
+
 //split v slice v splice
 
 //split is only for strings and returns arrays based off what it's using as the ',' prelacement
@@ -19,3 +21,18 @@ var newSlice = oldArray.slice(1, 3)[0];
 //console.log(oldArray);
 //console.log(newSlice);
 //console.log(newSlicer);
+
+var sent = "";
+
+var a = "Part 1 ";
+var b = "Part 2 ";
+var c = "Part 3";
+console.time();
+sent += a;
+console.log(sent);
+sent += b;
+console.log(sent);
+sent += c;
+
+console.log(sent);
+console.timeEnd();

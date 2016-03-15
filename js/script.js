a113c98612aa9565fe449da8cc30a9a107df6a40
samvnkauffman-laptop <samvnkauffman@gmail.com>
2016-03-15 16:15:08 -0400

    if

--- js/script.js
+++ js/script.js
@@ -3,4 +3,12 @@
 var one = "1";
 var two = "2";
 
-console.log(+one + +two);
+console.log(~~one + two);
+
+var startClock = new Date();
+
+console.log(+startClock);
+
+var tryer = 40;
+
+console.log(tryer >> 4);

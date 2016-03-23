d541b4a38b8b4b79de3275fe76c7b6829a8c3cf9
samvnkauffman-laptop <samvnkauffman@gmail.com>
2016-03-23 17:01:43 -0400

    first pascal

--- js/script.js
+++ js/script.js
@@ -1,20 +1,60 @@
-function arraySum(a) {
+/*jslint vars: true plusplus: true */
+/*jshint esversion: 6*/
+/*global $, document, Image, window, setTimeout, setInterval, clearInterval*/
 
-	// i will be an array, containing integers, strings and/or arrays like itself.
-	// Sum all the integers you find, anywhere in the nest of arrays.
-	var total;
-	var slot;
+function pascalMaker(numRows) {
+	let pascal = [[1]];
+	let rowCount = 1;
+	
+	while (rowCount < numRows) {
+		pascal[rowCount] = [];
+		let newRow = pascal[rowCount];
+		let prevRow = pascal[rowCount - 1];
 
-	Object.prototype.numCheck = function() {
-		if (this typeof "number") {
-			total += this;
+		for (let i = 0; i < prevRow.length + 1; i++) {
+			let befNum = prevRow[i - 1] || 0;
+			let aftNum = prevRow[i] || 0;
+			newRow[i] = befNum + aftNum;
 		}
+		rowCount++;
 	}
-
-	for (var i = 0; i <a.length; i++){
-		slot = a[i];
-		slot.numCheck();
-	}
-
-	return total;
+	
+	return numRows > 0 ? pascal : "Mr. Pascal needs at least one row.";
 }
+
+$.fn.addPascal = function(numRows) {
+	
+	let pascal = [[1]];
+	let rowCount = 1;
+
+	while (rowCount < numRows) {
+		pascal[rowCount] = [];
+		let newRow = pascal[rowCount];
+		let prevRow = pascal[rowCount - 1];
+
+		for (let i = 0; i < prevRow.length + 1; i++) {
+			let befNum = prevRow[i - 1] || 0;
+			let aftNum = prevRow[i] || 0;
+			newRow[i] = befNum + aftNum;
+		}
+		rowCount++;
+	}
+	
+	if (numRows > 0) {
+		$(this).append("<div class='pascal-box'></div>");
+		$(".pascal-box").css({"text-align" : "center", "margin-bottom": "20px"});
+		let stringPascal = pascal.join("<br>");
+		$(".pascal-box").last().html(stringPascal);
+	}
+
+};
+
+console.table(pascalMaker(5));
+console.table(pascalMaker(0));
+console.table(pascalMaker(-1));
+console.table(pascalMaker("cheese"));
+$("body").addPascal(8);
+$("body").addPascal(-3);
+$("body").addPascal("bill");
+$("body").addPascal(6);
+$("body").addPascal(20);

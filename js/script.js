5c40a2252df1ece42b4b59fa928d8373dd47f362
samvnkauffman-laptop <samvnkauffman@gmail.com>
2016-03-24 11:58:45 -0400

    final

--- js/script.js
+++ js/script.js
@@ -2,59 +2,39 @@
 /*jshint esversion: 6*/
 /*global $, document, Image, window, setTimeout, setInterval, clearInterval*/
 
-function pascalMaker(numRows) {
-	let pascal = [[1]];
-	let rowCount = 1;
-	
-	while (rowCount < numRows) {
-		pascal[rowCount] = [];
-		let newRow = pascal[rowCount];
-		let prevRow = pascal[rowCount - 1];
+function pattern(n) {
+	const constN = n;
 
-		for (let i = 0; i < prevRow.length + 1; i++) {
-			let befNum = prevRow[i - 1] || 0;
-			let aftNum = prevRow[i] || 0;
-			newRow[i] = befNum + aftNum;
-		}
-		rowCount++;
+	//make and set first line
+	let outputArray = [];
+	let firstLine = [];
+
+	for (let i = 0; i < constN; i++) {
+		firstLine.push(n);
+		n--;
 	}
+	outputArray[0] = firstLine;
+
+	//set additional lines
+	for (let i = 1; i < constN; i++) {
+		//define prev and next line
+		let prevLine = outputArray[i - 1];
+		let nextLine = prevLine.slice(0, constN - i);
+
+		//set actual next index to nextLine
+		outputArray[i] = nextLine;
+	}
+
+	//finally, convert each line to a string
+	for (let i = 0; i < constN; i++) {
+		outputArray[i] = outputArray[i].join("");
+	}
+
+	//lastly, break them up into seperate lines
+	let output = outputArray.join("\n");
 	
-	return numRows > 0 ? pascal : "Mr. Pascal needs at least one row.";
+	return output;
+
 }
 
-$.fn.addPascal = function(numRows) {
-	
-	let pascal = [[1]];
-	let rowCount = 1;
-
-	while (rowCount < numRows) {
-		pascal[rowCount] = [];
-		let newRow = pascal[rowCount];
-		let prevRow = pascal[rowCount - 1];
-
-		for (let i = 0; i < prevRow.length + 1; i++) {
-			let befNum = prevRow[i - 1] || 0;
-			let aftNum = prevRow[i] || 0;
-			newRow[i] = befNum + aftNum;
-		}
-		rowCount++;
-	}
-	
-	if (numRows > 0) {
-		$(this).append("<div class='pascal-box'></div>");
-		$(".pascal-box").css({"text-align" : "center", "margin-bottom": "20px"});
-		let stringPascal = pascal.join("<br>");
-		$(".pascal-box").last().html(stringPascal);
-	}
-
-};
-
-console.table(pascalMaker(5));
-console.table(pascalMaker(0));
-console.table(pascalMaker(-1));
-console.table(pascalMaker("cheese"));
-$("body").addPascal(8);
-$("body").addPascal(-3);
-$("body").addPascal("bill");
-$("body").addPascal(6);
-$("body").addPascal(20);
+console.log(pattern(101));
